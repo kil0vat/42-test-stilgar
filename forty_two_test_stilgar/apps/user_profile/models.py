398c09d415394d5e5028bad482e9764f9c0ce3de
Eugene “Stilgar“ Gilewski <developer@gilevski.name>
2012-02-18 16:00:09 +0200

    ticket:5 Tests.

--- forty_two_test_stilgar/apps/user_profile/models.py
+++ forty_two_test_stilgar/apps/user_profile/models.py
@@ -4,11 +4,12 @@ from forty_two_test_stilgar.helpers.model_helpers import ExtendedModel
 
 class Profile(ExtendedModel):
     """User profile model."""
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
     date_of_birth = models.DateField()
     bio = models.TextField()
     email = models.EmailField()
     jabber = models.EmailField()
-    skype = models.CharField(max_length=50)
+    skype = models.CharField(max_length=64)
     contacts = models.TextField()
+    image = models.ImageField(upload_to='profile', max_length=512, blank=True)
