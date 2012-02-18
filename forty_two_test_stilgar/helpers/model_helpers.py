ab09113f5796791d5467407fd3de24f89814e8c5
Eugene “Stilgar“ Gilewski <developer@gilevski.name>
2012-02-18 15:55:07 +0200

    Changes to helpers and dependant on them modules.

--- forty_two_test_stilgar/helpers/model_helpers.py
+++ forty_two_test_stilgar/helpers/model_helpers.py
@@ -5,10 +5,12 @@ from django.db.models import Model
 class ExtendedModel(Model):
     """Model with reusable custom methods."""
     def get_fields(self, exclude=('id',)):
-        """Returns list of all fields except "id"."""
-        # pylint: disable=E1101
-        return [getattr(self, field.name) \
-                for field in self._meta.fields if not field.name in exclude]
+        """Return dict of all fields except listed to be excluded and empty."""
+        fields = {}
+        for field in self._meta.fields:
+            if not field.name in exclude and getattr(self, field.name):
+                fields[field.name] = getattr(self, field.name)
+        return fields
 
     fields = property(get_fields)
     """List of all fields except "id"."""
