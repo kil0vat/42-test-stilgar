fe318189422d43ab5e497e355cc21060c7df4cad
Eugene “Stilgar“ Gilewski <developer@gilevski.name>
2012-02-18 16:00:55 +0200

    ticket:5 Implementation

--- forty_two_test_stilgar/apps/user_profile/views.py
+++ forty_two_test_stilgar/apps/user_profile/views.py
@@ -1,13 +1,86 @@
-"""Views for user profile app."""
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+"""Views for user profile app. Show data and edit data for staff."""
+import os.path
+from django.views.generic.simple import direct_to_template
+from django.contrib.auth.decorators import login_required, user_passes_test
+from forty_two_test_stilgar import settings
+from forty_two_test_stilgar.helpers.image_preview import \
+        temporarily_store_image, restore_stored_image, drop_stored_image
 from forty_two_test_stilgar.apps.user_profile.models import Profile
+from forty_two_test_stilgar.apps.user_profile.forms import ProfileEditForm
 
 
 def user_profile(request):
-    """Provides page with user profile information from Profile model."""
+    """Page with user profile information from Profile model."""
     profile = Profile.objects.all()[0]
-    # Not using generic view, because this is simplier (ticket:1#comment:80).
-    return render_to_response('user_profile_profile.html',
-            {'profile': profile},
-            context_instance=RequestContext(request))
+    # Not using object_detail generic view, because this is simpler -
+    # see (ticket:1#comment:80).
+    return direct_to_template(request,
+                             template='user_profile_profile.html',
+                             extra_context={'profile': profile})
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def edit_user_profile(request):
+    """User profile edit page."""
+    extra_context = {}
+    profile = Profile.objects.all()[0]
+    if request.method == 'POST':
+        image_preview_id = ''
+        stored_image = None
+        # Handle temporary soring image.
+        if ('image' in request.FILES) or \
+                (('forget_unsaved_image' in request.POST) and \
+                request.POST['forget_unsaved_image']):
+            drop_stored_image(request)
+            request.POST['forget_unsaved_image'] = False
+        if 'image' in request.FILES:
+            if 'preview' in request.POST:
+                image_id, image_path = temporarily_store_image(
+                        request.FILES['image'])
+                del request.FILES['image']
+                extra_context['image_preview'] = image_path
+                image_preview_id = image_id
+                request.session[image_preview_id] = \
+                        extra_context['image_preview']
+                extra_context['unsaved_changes'] = True
+        else:
+            try:
+                image_path = request.session[request.POST['image_preview_id']]
+                extra_context['image_preview'] = image_path
+                image_preview_id = request.POST['image_preview_id']
+                stored_image = os.path.join(settings.MEDIA_ROOT,
+                                            extra_context['image_preview'])
+            except KeyError:
+                pass
+        request.POST['image_preview_id'] = image_preview_id
+
+        # Handle action: preview or save.
+        if 'preview' in request.POST:
+            form = ProfileEditForm(request.POST, instance=profile)
+            if form.has_changed():
+                extra_context['unsaved_changes'] = True
+            if ('image-clear' in request.POST) and \
+                    request.POST['image-clear'] == 'on':
+                extra_context['persistant_image_clear'] = \
+                        request.POST['image-clear']
+            extra_context['debug'] = request.POST
+        elif 'save' in request.POST:
+            form = ProfileEditForm(request.POST, request.FILES,
+                                   instance=profile)
+            if form.is_valid():
+                if stored_image:
+                    image_dir = profile.image.field.upload_to
+                    stored_image = restore_stored_image(stored_image,
+                                                        image_dir)
+                form.save(stored_image=stored_image)
+                # FIXME: rain dance.
+                # Recreating form to update "Current value" of ImageField.
+                form = ProfileEditForm(request.POST, instance=profile)
+                drop_stored_image(request)
+                extra_context = {}
+    else:
+        form = ProfileEditForm(instance=profile)
+    extra_context['form'] = form
+    return direct_to_template(request,
+                             template='user_profile_edit.html',
+                             extra_context=extra_context)
--- forty_two_test_stilgar/urls.py
+++ forty_two_test_stilgar/urls.py
@@ -1,15 +1,24 @@
 """Django urlconf for forty_two_test_stilgar project."""
 from django.conf.urls.defaults import patterns, include, url
-from apps.user_profile.views import user_profile
+from django.conf.urls.static import static
+from django.contrib.auth.views import login, logout
+from forty_two_test_stilgar import settings
+from forty_two_test_stilgar.apps.user_profile.views import \
+        user_profile, edit_user_profile
 
 from django.contrib import admin
 admin.autodiscover()
 
 # pylint: disable=C0103
 urlpatterns = patterns('',
-    url(r'^$', user_profile),
+    url(r'^$', user_profile, name='frontpage'),
+    url(r'^edit-user-profile/$', edit_user_profile),
     url(r'^request-log/',
             include('forty_two_test_stilgar.apps.request_logger.urls')),
+    (r'^accounts/login/$',  login),
+    (r'^accounts/logout/$', logout, {'next_page': '/'}),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
