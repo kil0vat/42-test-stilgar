8736144e85bf2076148bfd3be58f2c83d1c3209d
Eugene “Stilgar“ Gilewski <developer@gilevski.name>
2012-02-28 17:34:52 +0200

    ticket:8 Improvement: tag now uses contenttypes framework.

--- forty_two_test_stilgar/apps/template_library/templatetags/admin_change_link.py
+++ forty_two_test_stilgar/apps/template_library/templatetags/admin_change_link.py
@@ -1,14 +1,15 @@
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django import template
+from django.contrib.contenttypes.models import ContentType
 
 register = template.Library()
-
 
 @register.simple_tag
 def admin_change_link(token):
     try:
-        app = token._meta.app_label
-        model = token._meta.module_name
+        content_type = ContentType.objects.get_for_model(token)
+        app = content_type.app_label
+        model = content_type.model
         url = reverse('admin:%s_%s_change' % (app, model), args=(token.id,))
     except:
         url = ''
