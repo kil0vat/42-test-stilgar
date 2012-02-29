be29e4c6696a2583b562f8b57173cab4a33f2c12
Eugene “Stilgar“ Gilewski <developer@gilevski.name>
2012-02-29 14:10:20 +0200

    ticket:16 Fix: request log list count changed to 10, sorting by priority now made within last 10 records.

--- forty_two_test_stilgar/apps/request_logger/urls.py
+++ forty_two_test_stilgar/apps/request_logger/urls.py
@@ -1,16 +1,10 @@
 """Request logger app's urlconf."""
 from django.conf.urls.defaults import patterns, url
 from forty_two_test_stilgar.apps.request_logger.views import \
-        simulate_http_status
+        request_list, simulate_http_status
 from django.views.generic import list_detail
 from forty_two_test_stilgar.apps.request_logger.models import Request
 
-REQUEST_LOG_LIST_INFO = {
-    'queryset': Request.objects.order_by('-priority', '-id'),
-    'template_object_name': 'request_record',
-    'template_name': 'request_logger_list.html',
-    'paginate_by': 20,
-}
 REQUEST_LOG_DETAIL_INFO = {
     'queryset': Request.objects.all(),
     'template_object_name': 'request_record',
@@ -20,10 +14,7 @@ REQUEST_LOG_DETAIL_INFO = {
 # pylint: disable=C0103
 urlpatterns = patterns('',
     url(r'^testing/(?P<status>\d{3})/$', simulate_http_status),
-    url(r'^$',
-        list_detail.object_list,
-        REQUEST_LOG_LIST_INFO,
-        name="request_log_list"),
+    url(r'^$', request_list, name="request_log_list"),
     url(r'^(?P<object_id>\d+)/$',
         list_detail.object_detail,
         REQUEST_LOG_DETAIL_INFO,
--- forty_two_test_stilgar/apps/request_logger/views.py
+++ forty_two_test_stilgar/apps/request_logger/views.py
@@ -1,7 +1,17 @@
-"""Request logger views. Helper for tests simulate_http_status(). Request
-lists are in app's urlconf."""
+"""Request logger views. Request list and helper for tests
+simulate_http_status(). Request detail view is in app's urlconf."""
 from django.http import HttpResponse
 import django.utils.simplejson as json
+from django.views.generic.simple import direct_to_template
+from forty_two_test_stilgar.apps.request_logger.models import Request
+
+
+def request_list(request):
+    """Show request list."""
+    requests = Request.objects.order_by('-id')[:10]
+    requests = sorted(requests, key=lambda x: x.priority, reverse=True)
+    return direct_to_template(request, template='request_logger_list.html',
+                              extra_context={'request_record_list': requests})
 
 
 def simulate_http_status(request, status):
